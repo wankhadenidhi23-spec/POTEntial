a2addcfd82f384f59c560a14ecb114a5971bf9cc
wankhadenidhi23-spec <wankhadenidhi23@gmail.com>
2026-09-26 10:51:50 +0530

    Update supabase-config.js with url and key only

--- BUSINESS DASHBOARD/supabase-config.js
+++ BUSINESS DASHBOARD/supabase-config.js
@@ -6,7 +6,3 @@ const SUPABASE_URL = "https://epedptuewukgferdpzjq.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_PpDvDuEQDqNirED5FNEZsA_p7wHVl8s";
 
 // Create Supabase client
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
